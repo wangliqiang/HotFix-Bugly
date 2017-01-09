caf8ed1f8167fbe1d202efa35fdd53d567511590
王立强 <wangliqiangorz@163.com>
2017-01-09 16:58:28 +0800

    add bugly hotfix

--- app/src/main/java/com/app/hotfix_bugly/MainActivity.java
+++ app/src/main/java/com/app/hotfix_bugly/MainActivity.java
@@ -1,13 +1,32 @@
 package com.app.hotfix_bugly;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mVersion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mVersion = (TextView) findViewById(R.id.version);
+        mVersion.setText(getVersion());
+    }
+
+    public String getVersion() {
+        try {
+            PackageManager manager = this.getPackageManager();
+            PackageInfo info = manager.getPackageInfo(this.getPackageName(), 0);
+            String version = info.versionName;
+            return "当前版本号：" + version;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "获取版本失败！！！";
     }
 }
