936427cd78c6eb5aeddcd026fdf267edf6c63596
王立强 <wangliqiangorz@163.com>
2017-01-11 09:53:13 +0800

    To fail in the end

--- app/src/main/java/com/app/hotfix_bugly/BugClass.java
+++ app/src/main/java/com/app/hotfix_bugly/BugClass.java
@@ -2,16 +2,13 @@ package com.app.hotfix_bugly;
 
 /**
  * 测试bug类.
- *
- * @author devilwwj
- * @since 2016/10/25
  */
 public class BugClass {
 
     public String bug() {
         // 这段代码会报空指针异常
-        String str = null;
-        int length = str.length();
+//        String str = null;
+//        int length = str.length();
         return "This is a fixed bug class";
     }
 }
--- app/src/main/java/com/app/hotfix_bugly/LoadBugClass.java
+++ app/src/main/java/com/app/hotfix_bugly/LoadBugClass.java
@@ -2,15 +2,11 @@ package com.app.hotfix_bugly;
 
 /**
  * 加载Bug的类.
- *
- * @author devilwwj
- * @since 2016/10/25
  */
 public class LoadBugClass {
 
     /**
      * 获取bug字符串.
-     *
      * @return 返回bug字符串
      */
     public static String getBugString() {
